4d29f98226427e2dc863d3f7b1ffacfbd67f6d17
kacper <106306274+kayYZ1@users.noreply.github.com>
2024-01-31 12:34:45 +0100

    test(credit-card): added test for generating number&cvv

--- src/credit-card/credit-card.service.spec.ts
+++ src/credit-card/credit-card.service.spec.ts
@@ -1,24 +1,61 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { getRepositoryToken } from '@nestjs/typeorm';
 
-import { CreditCardController } from './credit-card.controller';
+import { Repository } from 'typeorm';
+import { Provider as CardProvider } from './credit-card.enum';
 import { CreditCardService } from './credit-card.service';
 import { CreditCard } from './entities/credit-card.entity';
 
 describe('CreditCardService', () => {
   let service: CreditCardService;
+  let creditCardRespository: Repository<CreditCard>;
+
+  const CREDITCARD_REPOSITORY_TOKEN = getRepositoryToken(CreditCard);
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      imports: [TypeOrmModule.forFeature([CreditCard])],
-      controllers: [CreditCardController],
-      providers: [CreditCardService],
+      providers: [
+        CreditCardService,
+        {
+          provide: CREDITCARD_REPOSITORY_TOKEN,
+          useValue: {
+            create: jest.fn(),
+            findOneBy: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     service = module.get<CreditCardService>(CreditCardService);
+    creditCardRespository = module.get<Repository<CreditCard>>(
+      CREDITCARD_REPOSITORY_TOKEN,
+    );
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('creditCardRepository should be define', () => {
+    expect(creditCardRespository).toBeDefined();
+  });
+
+  describe('creditCard generation', () => {
+    it('should generate a valid credit card number', () => {
+      const mockCreditCardNumber = service.generateCardNumber(
+        CardProvider.VISA,
+      );
+
+      expect(mockCreditCardNumber).toHaveLength(16);
+    });
+
+    it('should generate a valid cvv number', () => {
+      const mockCreditCardNumber = service.generateCardNumber(
+        CardProvider.MASTERCARD,
+      );
+      const mockCVV = service.generateCVV(mockCreditCardNumber);
+
+      expect(mockCVV).toHaveLength(3);
+    });
   });
 });
