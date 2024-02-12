989f8cccb5b213171658bc924e67e06fd8b42ffd
kayYZ1 <overek7133@gmail.com>
2024-02-12 12:05:58 +0100

    test(loan-calc): add mortgage calc test

--- src/loan-calculator/loan-calculator.service.spec.ts
+++ src/loan-calculator/loan-calculator.service.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CashLoanDto } from './dto/cashLoan.dto';
+import { MortgageLoanDto } from './dto/mortgageLoan.dto';
 import { LoanCalculatorService } from './loan-calculator.service';
 
 describe('LoanCalculatorService', () => {
@@ -17,16 +18,29 @@ describe('LoanCalculatorService', () => {
     expect(service).toBeDefined();
   });
 
-  it('should return correct monthly rate', async () => {
+  it('should return correct monthly rate (cash)', () => {
     const mockDto: CashLoanDto = {
       loanAmount: 20000,
       installments: 12,
-      monthPeriodRepayment: 24,
-      provision: 0,
+      paymentPeriod: 24, //months
       interestRate: 0.08,
     };
-    const result = await service.cashLoanCalculator(mockDto);
 
-    expect(result).toBe(904.55);
+    const rate = service.cashLoanCalculator(mockDto);
+
+    expect(rate).toBe(904.55);
+  });
+
+  it('should return correct stable rate (mortgage)', () => {
+    const mockDto: MortgageLoanDto = {
+      loanAmount: 300000,
+      installments: 12,
+      paymentPeriod: 25, //years
+      interestRate: 0.04,
+    };
+
+    const rate = service.mortgageCalculator(mockDto);
+
+    expect(rate).toBe(1583.51);
   });
 });
